d00f2334a5a4d70f738cb77446bcc7325af4b6a7
Inna Didenko <innadidenko29@gmail.com>
2023-08-10 22:19:02 +0300

    add file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+ import ReactDOM from 'react-dom/client';
+ import { App } from 'components/App';
 import './index.css';
+
+import user from "../assets/user.json";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App user={user} />
   </React.StrictMode>
 );
